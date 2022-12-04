216b1959a8ae3db0fb8f72d0da15db009323fb8c
ZCTMDC <zctmdc@outlook.com>
2022-12-05 05:40:52 +0800

    init_mariadb_repo

--- mariadb/init_mariadb_repo.sh
+++ mariadb/init_mariadb_repo.sh
@@ -1,4 +1,4 @@
-echo "
+cat > /etc/yum.repos.d/mariadb.repo << EOF
 # MariaDB ${VERSION} CentOS repository list - created $(date -u)
 # http://downloads.mariadb.org/mariadb/repositories/
 [mariadb]
@@ -6,4 +6,4 @@ name = MariaDB
 baseurl = http://yum.mariadb.org/${VERSION}/centos7-amd64
 gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1
-" > /etc/yum.repos.d/mariadb.repo
+EOF
